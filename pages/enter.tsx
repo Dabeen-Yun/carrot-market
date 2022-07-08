8012736e08c01898de99019e58af251edfe0235b
Dabeen-Yun <90141741+Dabeen-Yun@users.noreply.github.com>
2022-07-08 11:48:46 +0900

    complete end page and add index page

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,34 +1,82 @@
 import { useState } from "react";
 
+function cls(...classnames: string[]) {
+  return classnames.join(" ");
+}
+
 export default function Enter() {
   const [method, setMethod] = useState<"email" | "phone">("email");
   const onEmailClick = () => setMethod("email");
   const onPhoneClick = () => setMethod("phone");
   return (
-    <div className="pt-8">
-      <h3 className="font-extrabold text-3xl flex justify-center">Enter to Carrot</h3>
+    <div className="mt-16 w-11/12 m-auto">
+      <h3 className="font-extrabold text-3xl flex justify-center">
+        Enter to Carrot
+      </h3>
       <div>
-        <div className="p-9">
-          <h5 className="text-sm font-semibold text-gray-500 flex justify-center">Enter using:</h5>
-          <div className="flex justify-around border-b">
-            <button onClick={onEmailClick} className="focus:border-x-emerald-600">Email address</button>
-            <button onClick={onPhoneClick}>Phone number</button>
+        <div>
+          <h5 className="p-8 text-sm font-semibold text-gray-500 flex justify-center">
+            Enter using:
+          </h5>
+          <div className="flex justify-around border-b w-full mb-7">
+            <button
+              className={cls(
+                "pb-4 font-medium", //기본적으로 들어가는 classname
+                method === "email"
+                  ? "border-b-2 border-orange-500 text-orange-400" // method가 email일 때 들어가는 classname
+                  : "border-transparent" //method가 phone일 때 들어가는 classname
+              )}
+              onClick={onEmailClick}
+            >
+              Email address
+            </button>
+            <button
+              className={cls(
+                "pb-4 font-medium",
+                method === "phone"
+                  ? "border-b-2 border-orange-500 text-orange-400"
+                  : "border-transparent"
+              )}
+              onClick={onPhoneClick}
+            >
+              Phone number
+            </button>
           </div>
+        </div>
+        <div>
           <form>
-            <label>
+            <label className="font-semibold text-sm">
               {method === "email" ? "Email address" : null}
               {method === "phone" ? "Phone number" : null}
             </label>
-            <div>
-              {method === "email" ? <input type="email" required /> : null}
+            <div className="mt-3">
+              {method === "email" ? (
+                <input
+                  className="appearance-none w-full rounded-lg 
+                  placeholder:text-gray-300 
+                  focus:border-orange-500 focus:border-2 foucs:outline-none focus:ring-0
+                  hover:brightness-90"
+                  type="email"
+                  required
+                />
+              ) : null}
               {method === "phone" ? (
-                <div>
-                  <span>+82</span>
-                  <input type="number" required />
+                <div className="flex rounded-md shadow-sm">
+                  <span className="flex justify-center items-center border border-gray-300 px-3 rounded-l-md bg-gray-100">
+                    +82
+                  </span>
+                  <input
+                    className="flex w-full rounded-r-md appearance-none py-2 border-gray-300
+                    placeholder:text-gray-300 
+                    focus:border-orange-500 focus:border-2 foucs:outline-none focus:focus:ring-0
+                      hover:brightness-90"
+                    type="number"
+                    required
+                  />
                 </div>
               ) : null}
             </div>
-            <button>
+            <button className="bg-orange-500 rounded-lg border border-gray-600 shadow-lg text-white text-sm font-semibold w-full mt-7 py-2 focus:outline-none focus:brightness-90">
               {method === "email" ? "Get login link" : null}
               {method === "phone" ? "Get one-time password" : null}
             </button>
@@ -38,12 +86,14 @@ export default function Enter() {
               <div>
                 <div />
               </div>
-              <div>
-                <span>Or enter with</span>
+              <div className="mt-10 flex justify-center">
+                <span className="font-semibold text-xs text-gray-500">
+                  Or enter with
+                </span>
               </div>
             </div>
-            <div>
-              <button>
+            <div className="flex justify-between mt-5">
+              <button className="border border-gray-300 w-1/2 mr-4 rounded-lg flex items-center justify-center shadow-sm">
                 <svg
                   className="w-5 h-5"
                   aria-hidden="true"
@@ -53,7 +103,16 @@ export default function Enter() {
                   <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
                 </svg>
               </button>
-              <button className="f">트위터?</button>
+              <button className="border border-gray-300 w-1/2 rounded-lg p-3 flex justify-center shadow-sm">
+                <svg
+                  className="w-6 h-6"
+                  aria-hidden="true"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M 8 0 C 3.58 0 0 3.58 0 8 c 0 3.54 2.29 6.53 5.47 7.59 c 0.4 0.07 0.55 -0.17 0.55 -0.38 c 0 -0.19 -0.01 -0.82 -0.01 -1.49 c -2.01 0.37 -2.53 -0.49 -2.69 -0.94 c -0.09 -0.23 -0.48 -0.94 -0.82 -1.13 c -0.28 -0.15 -0.68 -0.52 -0.01 -0.53 c 0.63 -0.01 1.08 0.58 1.23 0.82 c 0.72 1.21 1.87 0.87 2.33 0.66 c 0.07 -0.52 0.28 -0.87 0.51 -1.07 c -1.78 -0.2 -3.64 -0.89 -3.64 -3.95 c 0 -0.87 0.31 -1.59 0.82 -2.15 c -0.08 -0.2 -0.36 -1.02 0.08 -2.12 c 0 0 0.67 -0.21 2.2 0.82 c 0.64 -0.18 1.32 -0.27 2 -0.27 c 0.68 0 1.36 0.09 2 0.27 c 1.53 -1.04 2.2 -0.82 2.2 -0.82 c 0.44 1.1 0.16 1.92 0.08 2.12 c 0.51 0.56 0.82 1.27 0.82 2.15 c 0 3.07 -1.87 3.75 -3.65 3.95 c 0.29 0.25 0.54 0.73 0.54 1.48 c 0 1.07 -0.01 1.93 -0.01 2.2 c 0 0.21 0.15 0.46 0.55 0.38 A 8.013 8.013 0 0 0 16 8 c 0 -4.42 -3.58 -8 -8 -8 Z" />
+                </svg>
+              </button>
             </div>
           </div>
         </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,80 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div className="dark:md:hover:bg-teal-400"></div>;
+  return (
+    <div>
+      {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
+        <div
+          key={i}
+          className="py-8 px-4 flex flex-row justify-between border-b shadow-sm select-none"
+        >
+          <div className="flex flex-row space-x-4">
+            <div className="h-24 w-24 bg-gray-400 rounded-lg shadow-sm" />
+            <div className="flex flex-col justify-center">
+              <h3 className="font-semibold text-md">New iPhone 14</h3>
+              <span className="text-gray-500 text-sm">Black</span>
+              <span className="text-lg font-bold mt-3">$95</span>
+            </div>
+          </div>
+          <div className="flex flex-row space-x-1 items-end text-gray-500">
+            <div className="flex flex-row space-x-1 mr-5">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                />
+              </svg>
+              <span>1</span>
+            </div>
+            <div className="flex flex-row space-x-1 mr-5">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+                />
+              </svg>
+              <span>1</span>
+            </div>
+          </div>
+        </div>
+      ))}
+      <button
+        className="fixed bottom-24 right-5 bg-orange-400 rounded-full p-3 text-white
+      hover:bg-orange-500"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth={2}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 4v16m8-8H4"
+          />
+        </svg>
+      </button>
+    </div>
+  );
 };
 
 export default Home;
